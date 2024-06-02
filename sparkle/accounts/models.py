804dbb2b5a122151ac8afc38c4437b43d7b5296c
minaalipur <miinalipour@gmail.com>
2024-06-02 17:31:22 +0330

    creating models

--- sparkle/accounts/models.py
+++ sparkle/accounts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+#from shop.models import Product
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=100, unique=True)
+    full_name = models.CharField(max_length=100)    
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    #likes = models.ManyToManyField(Product, blank=True, related_name='likes')
+    is_seller = models.BooleanField(default=False)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name']
+
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+  #  def get_likes_count(self):
+  #     return self.likes.count()
