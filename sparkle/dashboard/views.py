4e46dd8fc7a3a83bd1639f4d9fc4d23b45825dfe
minaalipur <miinalipour@gmail.com>
2024-07-02 10:50:00 +0330

    handling deleting & editing a product,adding a new category , displaying order and order details

--- sparkle/dashboard/views.py
+++ sparkle/dashboard/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
 from django.http import Http404
 
 from shop.models import Product
-from .forms import AddProductForm
+from accounts.models import User
+from orders.models import Order, OrderItem
+from .forms import AddProductForm, AddCategoryForm, EditProductForm
+
 
 def is_manager(user):
     try:
@@ -14,6 +17,7 @@ def is_manager(user):
         return True
     except:
         raise Http404
+
 
 @user_passes_test(is_manager)
 @login_required
@@ -36,3 +40,59 @@ def add_product(request):
         form = AddProductForm()
     context = {'title':'Add Product', 'form':form}
     return render(request, 'add_product.html', context)
+
+
+@user_passes_test(is_manager)
+@login_required
+def delete_product(request, id):
+    product = Product.objects.filter(id=id).delete()
+    messages.success(request, 'product has been deleted!', 'success')
+    return redirect('dashboard:products')
+
+
+@user_passes_test(is_manager)
+@login_required
+def edit_product(request, id):
+    product = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        form = EditProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product has been updated', 'success')
+            return redirect('dashboard:products')
+    else:
+        form = EditProductForm(instance=product)
+    context = {'title': 'Edit Product', 'form':form}
+    return render(request, 'edit_product.html', context)
+
+
+@user_passes_test(is_manager)
+@login_required
+def add_category(request):
+    if request.method == 'POST':
+        form = AddCategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Category added Successfuly!')
+            return redirect('dashboard:add_category')
+    else:
+        form = AddCategoryForm()
+    context = {'title':'Add Category', 'form':form}
+    return render(request, 'add_category.html', context)
+
+
+@user_passes_test(is_manager)
+@login_required
+def orders(request):
+    orders = Order.objects.all()
+    context = {'title':'Orders', 'orders':orders}
+    return render(request, 'orders.html', context)
+
+
+@user_passes_test(is_manager)
+@login_required
+def order_detail(request, id):
+    order = Order.objects.filter(id=id).first()
+    items = OrderItem.objects.filter(order=order).all()
+    context = {'title':'order detail', 'items':items, 'order':order}
+    return render(request, 'order_detail.html', context)
