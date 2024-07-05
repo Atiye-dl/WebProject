dd7e38a4d69839b46b423b3714d9a559f6610439
minaalipur <miinalipour@gmail.com>
2024-07-05 13:42:16 +0330

    changing the host user email

--- sparkle/sparkle/settings.py
+++ sparkle/sparkle/settings.py
@@ -136,5 +136,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'username@example.com'
-EMAIL_HOST_PASSWORD = 'your-password'
+EMAIL_HOST_USER = 'hosthost587@gmail.com'
+EMAIL_HOST_PASSWORD = 'host587HOST'
