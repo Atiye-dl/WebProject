1f81933e260cc42b41dadde7807fffa5e0c79520
minaalipur <miinalipour@gmail.com>
2024-07-13 14:24:28 +0330

    adding deactive sers action

--- sparkle/accounts/admin.py
+++ sparkle/accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib import messages
 from .models import User
 
 class UserAdmin(admin.ModelAdmin):
@@ -6,8 +7,15 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = ('is_active', 'is_admin', 'is_manager')
     search_fields = ('email', 'full_name')
     ordering = ('email',)
+    actions = ['deactivate_users']
 
     def has_module_perms(self, request, obj=None):
         return request.user.is_admin
+
+    def deactivate_users(self, request, queryset):
+        updated = queryset.update(is_active=False)
+        self.message_user(request, f'{updated} user(s) successfully deactivated.', messages.SUCCESS)
+
+    deactivate_users.short_description = 'Deactivate selected users'
 
 admin.site.register(User, UserAdmin)
